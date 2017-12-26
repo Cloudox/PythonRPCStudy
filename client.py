9211ed3303075bb1a8add632411950d38d21f89e
Cloudox <cloudox@foxmail.com>
2017-12-26 16:18:18 +0800

    增加文件传输功能

--- client.py
+++ client.py
@@ -1,6 +1,15 @@
 from xmlrpc.client import ServerProxy
+import xmlrpc.client
 
 if __name__ == '__main__':
-    server = ServerProxy("http://localhost:8888")
+    server = ServerProxy("http://localhost:8888", allow_none=True)
     print (server.get_string("cloudox"))
     print (server.add(8, 8))
+    # 上传文件
+    put_handle = open("girl.jpg", 'rb')
+    server.image_put(xmlrpc.client.Binary(put_handle.read()))
+    put_handle.close()
+    # 下载文件
+    get_handle = open("get_boy.jpg", 'wb')
+    get_handle.write(server.image_get().data)
+    get_handle.close()
--- server.py
+++ server.py
@@ -2,23 +2,41 @@
 
 from xmlrpc.server import SimpleXMLRPCServer
 from socketserver import ThreadingMixIn
+import xmlrpc.client
 
 
 class ThreadXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
     pass
 
+
 # 调用函数1
 def respon_string(str):
     return "get string:%s"%str
+
 
 # 调用函数2
 def add(x, y):
     return x + y
 
 
+# 供客户端下载文件
+def image_get():
+    handle = open("boy.jpg", 'rb')
+    return xmlrpc.client.Binary(handle.read())
+
+
+# 供客户端上传文件
+def image_put(data):
+    handle = open("get_girl.jpg", 'wb')
+    handle.write(data.data)
+    handle.close()
+
+
 if __name__ == '__main__':
-    server = ThreadXMLRPCServer(('localhost', 8888)) # 初始化
+    server = ThreadXMLRPCServer(('localhost', 8888), allow_none=True) # 初始化
     server.register_function(respon_string, "get_string") # 注册函数1
     server.register_function(add, 'add') # 注册函数2
+    server.register_function(image_put, 'image_put')
+    server.register_function(image_get, 'image_get')
     print ("Listening for Client")
     server.serve_forever() # 保持等待调用状态
